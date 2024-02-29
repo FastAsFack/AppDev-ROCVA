c94a160518ed57029231c374b737efbcd41507e9
Tijn <jemoderiseenplopkoek3451@gmail.com>
2024-02-29 14:35:37 +0100

    First code for starting
    Here we have a function print_menu its for printing the menu and then we have the main function this makes use of the print_menu function and checks what the user input is and does the appropriate actions for the right input

---
+++ main.py
@@ -0,0 +1,26 @@
+def print_menu():
+    print("\n1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Exit")
+
+def main():
+    while True:
+        print_menu()
+        choice = input('Choose an option: ')
+        if choice == '1':
+            pass  # Replace with code for option 1
+        elif choice == '2':
+            pass  # Replace with code for option 2
+        elif choice == '3':
+            pass  # Replace with code for option 3
+        elif choice == '4':
+            pass  # Replace with code for option 4
+        elif choice == '5':
+            break
+        else:
+            print('Invalid choice')
+
+if __name__ == '__main__':
+    main()
